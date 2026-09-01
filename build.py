1553fa55ed371e7d1bc1b62fc24803ff061a61d8
unimauro <unimauro@gmail.com>
2026-09-01 10:37:58 -0500

    Deps: actualiza Font Awesome 6.4.0 -> 6.7.2 (CDN) y regenera los 8 idiomas

--- build.py
+++ build.py
@@ -470,7 +470,7 @@ def render(loc, S):
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-170L71ZZK1"></script>
     <script>
